ba9a4a36716298682fb15494d608ca50fb1adc3a
Paul Arthur <paul.arthur@flowerysong.com>
2022-03-08 16:34:14 +0000

    test: fix deprecation warning
      /home/ec2-user/.local/lib/python3.7/site-packages/_pytest/nodes.py:140:
      PytestRemovedIn8Warning: The (fspath: py.path.local) argument to CMockaFile is
      deprecated.
       Please use the (path: pathlib.Path) argument instead.
         See https://docs.pytest.org/en/latest/deprecations.html#fspath-argument-for-node-constructors-replaced-with-pathlib-path
             return super().__call__(*k, **kw)

--- test/conftest.py
+++ test/conftest.py
@@ -12,9 +12,9 @@ from email.mime.text import MIMEText
 import pytest
 
 
-def pytest_collect_file(parent, path):
-    if os.access(str(path), os.X_OK) and path.basename.startswith('cmocka_'):
-        return CMockaFile.from_parent(parent, fspath=path)
+def pytest_collect_file(parent, file_path):
+    if os.access(str(file_path), os.X_OK) and file_path.name.startswith('cmocka_'):
+        return CMockaFile.from_parent(parent, path=file_path)
 
 
 class CMockaFile(pytest.File):
@@ -45,9 +45,9 @@ class CMockaFile(pytest.File):
 
 
 class CMockaItem(pytest.Item):
-    def __init__(self, parent, line, output=None):
+    def __init__(self, *, line, output=None, **kwargs):
         name = line.split(' - ')[1]
-        super(CMockaItem, self).__init__(name, parent)
+        super(CMockaItem, self).__init__(name, **kwargs)
         self.line = line
         self.output = output
 
